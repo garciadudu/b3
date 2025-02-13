6ba38c8df0a4cef5764a8d98eeec014c20be0a5c
Eduardo Garcia da Silva <eduardo.garciadasilva@gmail.com>
2025-02-13 12:16:21 -0300

    Initial frontend

---
+++ template/frontend/b3/src/app/cdb/cdb.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cdb',
+  imports: [],
+  templateUrl: './cdb.component.html',
+  styleUrl: './cdb.component.css'
+})
+export class CDBComponent {
+
+}
